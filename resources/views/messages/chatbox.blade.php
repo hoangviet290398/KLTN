0430c906cf457edd64ae686627f34c8c7be20619
Quy Tran <quytrandev@gmail.com>
2020-05-09 20:50:48 +0700

    Update message system UI

--- resources/views/messages/chatbox.blade.php
+++ resources/views/messages/chatbox.blade.php
@@ -1,54 +1,64 @@
-    <div id="messages" >
-
-    <div id="chat-box" class="px-4 py-5 chat-box bg-white">
-        <!-- Sender Message-->
-        @foreach($messages as $message)
-          @if($message->from_user == Auth::user()->id)
-       
-        <div class="media w-50 ml-auto mb-3">
-          <div class="media-body">
-            <div class="bg-primary rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-white">{{$message->message}}</p>
+    <div id="messages">
+        <div class="bg-gray px-4 py-2 bg-white border-bottom">
+            <!-- Receiver name -->
+            <div class="row">
+                <img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user"
+                    width="60" class="rounded-circle">
+                <p id="target_name" class="h4 mb-0 px-2 py-3"></p>
             </div>
-            <p class="small text-muted">{{date("H:i | F d, Y", strtotime($message->created_at))}}</p>
-          </div>
         </div>
-        <!-- Reciever Message-->
-        @else
-        <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-          <div class="media-body ml-3">
-            <div class="bg-light rounded py-2 px-3 mb-2">
-              <p   class="text-small mb-0 text-muted">{{$message->message}}</p>
-            </div>
-            <p class="small text-muted">{{date("H:i | F d, Y", strtotime($message->created_at))}}</p>
-          </div>
-        </div>
-        @endif
-        @endforeach
-       
-      </div>
-      <!-- Typing area -->
-      <form action="#" class="bg-light">
-        <div class="input-group">
-          <input id="typing-area" type="text" placeholder="Type a message" aria-describedby="button-addon2" autofocus class="form-control rounded-0 border-0 py-4 bg-light">
-          <div class="input-group-append">
-            <button id="button-addon2" type="submit" class="btn btn-link"> <i class="fa fa-paper-plane"></i></button>
-          </div>
-        </div>
-      </form>
+        <div id="chat-box" class="px-4 py-5 chat-box bg-white">
+            <!-- Sender Message-->
+            @foreach($messages as $message)
+            @if($message->from_user == Auth::user()->id)
 
-    </div>    
-      
+            <div class="media w-50 ml-auto mb-3">
+                <div class="media-body">
+                    <div class="bg-primary rounded py-2 px-3 mb-2">
+                        <p class="text-small mb-0 text-white">{{$message->message}}</p>
+                    </div>
+                    <p class="small text-muted">{{date("H:i | F d, Y", strtotime($message->created_at))}}</p>
+                </div>
+            </div>
+            <!-- Reciever Message-->
+            @else
+            <div class="media w-50 mb-3"><img
+                    src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user"
+                    width="50" class="rounded-circle">
+                <div class="media-body ml-3">
+                    <div class="bg-light rounded py-2 px-3 mb-2">
+                        <p class="text-small mb-0 text-muted">{{$message->message}}</p>
+                    </div>
+                    <p class="small text-muted">{{date("H:i | F d, Y", strtotime($message->created_at))}}</p>
+                </div>
+            </div>
+            @endif
+            @endforeach
+
+        </div>
+        <!-- Typing area -->
+        <form action="#" class="">
+            <div class="input-group pl-4 pr-2 py-3">
+                <input id="typing-area" type="text" placeholder="Type a message" aria-describedby="button-addon2"
+                    autofocus class="form-control rounded-0 border-0 py-4 bg-light">
+                <div class="input-group-append pl-1">
+                    <button id="button-addon2" type="submit" class="btn btn-link"onClick="sendMessageOnClick();"><i
+                            class="fa fa-paper-plane" ></i></button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.min.js">
-    
+
     </script>
     <script>
-    // make a function to scroll down auto
-    var objDiv = document.getElementById("chat-box");
-    objDiv.scrollTop = objDiv.scrollHeight;
+// make a function to scroll down auto
+var objDiv = document.getElementById("chat-box");
+objDiv.scrollTop = objDiv.scrollHeight;
 
-    $(document).on("keypress", function() {
+$(document).on("keypress", function() {
     $("#typing-area").focus();
-    });
+});
     </script>
-
--- resources/views/messages/messages.blade.php
+++ resources/views/messages/messages.blade.php
@@ -12,19 +12,19 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <style>
     .pending {
-            position: absolute;
-            left: 13px;
-            top: 9px;
-            background: #FF6347;
-            margin: 0;
-            border-radius: 50%;
-            width: 18px;
-            height: 18px;
-            line-height: 18px;
-            padding-left: 5px;
-            color: #ffffff;
-            font-size: 12px;
-        }
+        position: absolute;
+        left: 13px;
+        top: 9px;
+        background: #FF6347;
+        margin: 0;
+        border-radius: 50%;
+        width: 18px;
+        height: 18px;
+        line-height: 18px;
+        padding-left: 5px;
+        color: #ffffff;
+        font-size: 12px;
+    }
     </style>
 </head>
 
@@ -32,20 +32,24 @@
     <div class="wrapper rounded-lg overflow-x-hidden shadow">
         <div class="row">
             <!-- Users box-->
-            <div class="col-3 px-0">
+            <div class="col-3 px-0 border-right">
                 <div class="bg-white">
 
-                    <div class="bg-gray px-4 py-2 bg-light">
-                        <p class="h5 mb-0 py-1">Recent</p>
-                    </div>
+                    <div class="px-4 py-2  ">
+                        <div class="row px-2 align-center">
+                            <img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
+                                alt="user" width="60" class="rounded-circle">
+                            <p class="h4 mb-0 px-2 py-3">{{Auth::user()->fullname}} (you)</p>
+                        </div>
 
+                    </div>
                     <div class="messages-box">
                         <div class="list-group rounded-0">
 
                             @foreach($users as $user)
-                            <a id="{{$user->id}}" href="#"
+                            <a id="{{$user->id}}" data-receiverName="{{$user->fullname}}" href="#"
                                 class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                                
+
                                 <span class="pending">1</span>
                                 <div class="media"><img
                                         src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
@@ -69,7 +73,7 @@
                 </div>
             </div>
             <!-- Chat Box-->
-            <div class="col-6 px-0">
+            <div class="col-6 px-0 border-right">
                 @include('messages.chatbox')
 
                 <!-- Right side bar -->
@@ -81,58 +85,7 @@
                         <p class="h5 mb-0 py-1">Recent</p>
                     </div>
 
-                    <div class="messages-box">
-                        <div class="list-group rounded-0">
-                            <a class="list-group-item list-group-item-action active text-white rounded-0">
-                                <div class="media"><img
-                                        src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
-                                        alt="user" width="50" class="rounded-circle">
-                                    <div class="media-body ml-4">
-                                        <div class="d-flex align-items-center justify-content-between mb-1">
-                                            <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">25
-                                                Dec</small>
-                                        </div>
-                                        <p class="font-italic mb-0 text-small">Lorem ipsum dolor sit amet, consectetur
-                                            adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                                    </div>
-                                </div>
-                            </a>
 
-                            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                                <div class="media"><img
-                                        src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
-                                        alt="user" width="50" class="rounded-circle">
-                                    <div class="media-body ml-4">
-                                        <div class="d-flex align-items-center justify-content-between mb-1">
-                                            <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">14
-                                                Dec</small>
-                                        </div>
-                                        <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet,
-                                            consectetur. incididunt ut labore.</p>
-                                    </div>
-                                </div>
-                            </a>
-
-
-
-                            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-                                <div class="media"><img
-                                        src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg"
-                                        alt="user" width="50" class="rounded-circle">
-                                    <div class="media-body ml-4">
-                                        <div class="d-flex align-items-center justify-content-between mb-3">
-                                            <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">21
-                                                Aug</small>
-                                        </div>
-                                        <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet,
-                                            consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -143,16 +96,21 @@
     var receiver_id = '';
     var my_id = '{{ Auth::user()->id }}';
 
+    var receiver_name = '';
+    var message = '';
+
     // ajax setup form csrf token
     $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 
-        
+
     $('.list-group-item').click(function() {
         $('.list-group-item').removeClass('active list-group-item-light');
+        receiver_name = $(this).attr('data-receiverName');
+
 
         $(this).addClass('active');
 
@@ -165,65 +123,90 @@
             success: function(data) {
                 //alert(data);
                 $('#messages').html(data);
+                //appear current receiver 
+                replace = document.getElementById("target_name");
+                replace.innerHTML = receiver_name;
             }
         });
     });
 
-    $(document).on('keyup click', '.input-group input', function (e) {
-            var message = $(this).val();
-            // check if enter key is pressed and message is not null also receiver is selected
-             if (e.keyCode == 13 && message != '' && receiver_id != '') {
-                 alert(message);
-                 $(this).val(''); // while pressed enter text box will be empty
-                
-                var datastr = "receiver_id=" + receiver_id + "&message=" + message;
-                $.ajax({
-                    type: "post",
-                    url: "messages", // need to create this post route
-                    data: datastr,
-                    cache: false,
-                    success: function (data) {
-                    },
-                    error: function (jqXHR, status, err) {
-                    },
-                    complete: function () {
-                        scrollToBottomFunc();
-                    }
-                })
-            }
-        });
-    
+    function sendMessageOnClick() {
+        if (message == '') {
+
+            alert('Message cannot be empty!');
+        } else if (receiver_id == '') {
+            alert('Choose a receiver to send message!');
+
+        } else if(message !='' && receiver_id!='') {
+            alert(message);
+            $('.input-group input').val(''); // while pressed enter text box will be empty
+
+            var datastr = "receiver_id=" + receiver_id + "&message=" + message;
+            $.ajax({
+                type: "post",
+                url: "messages", // need to create this post route
+                data: datastr,
+                cache: false,
+                success: function(data) {},
+                error: function(jqXHR, status, err) {},
+                complete: function() {
+                    //scrollToBottomFunc();
+                }
+            })
+        }
+
+    }
+    $(document).on('keyup', '.input-group input', function(e) {
+        message = $(this).val();
+        // check if enter key is pressed and message is not null also receiver is selected
+        if (e.keyCode == 13 && message != '' && receiver_id != '') {
+            alert(message);
+            $(this).val(''); // while pressed enter text box will be empty
+
+            var datastr = "receiver_id=" + receiver_id + "&message=" + message;
+            $.ajax({
+                type: "post",
+                url: "messages", // need to create this post route
+                data: datastr,
+                cache: false,
+                success: function(data) {},
+                error: function(jqXHR, status, err) {},
+                complete: function() {
+                    //scrollToBottomFunc();
+                }
+            })
+        }
+    });
+
     // Enable pusher logging - don't include this in production
     Pusher.logToConsole = true;
 
     var pusher = new Pusher('0d5466969582ad7a648f', {
-      cluster: 'ap1',
-      forceTLS:true
+        cluster: 'ap1',
+        forceTLS: true
     });
 
     var channel = pusher.subscribe('my-channel');
     channel.bind('my-event', function(data) {
-      //alert(JSON.stringify(data));
-    
-      if (my_id == data.from_user) {
-                $('#' + data.to_user).click();
-            } else if (my_id == data.to_user) {
-                if (receiver_id == data.from_user) {
-                    // if receiver is selected, reload the selected user ...
-                    $('#' + data.from_user).click();
+        //alert(JSON.stringify(data));
+
+        if (my_id == data.from_user) {
+            $('#' + data.to_user).click();
+        } else if (my_id == data.to_user) {
+            if (receiver_id == data.from_user) {
+                // if receiver is selected, reload the selected user ...
+                $('#' + data.from_user).click();
+            } else {
+                // if receiver is not seleted, add notification for that user
+                var pending = parseInt($('#' + data.from_user).find('.pending').html());
+                if (pending) {
+                    $('#' + data.from_user).find('.pending').html(pending + 1);
                 } else {
-                    // if receiver is not seleted, add notification for that user
-                    var pending = parseInt($('#' + data.from_user).find('.pending').html());
-                    if (pending) {
-                        $('#' + data.from_user).find('.pending').html(pending + 1);
-                    } else {
-                        $('#' + data.from_user).append('<span class="pending">1</span>');
-                    }
+                    $('#' + data.from_user).append('<span class="pending">1</span>');
                 }
             }
-    });    
-
-    
+        }
+    });
     </script>
 </body>
 
